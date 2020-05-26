7318d9fe6adba6df7a56e5e2d3c23b44019d0171
Artie Fischer <artie@curetechnologies.us>
2020-05-26 17:01:29 -0600

    wussup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ListComponent } from './list/list.component';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import {MatButtonModule, MatCardModule, MatChipsModule, MatFormFieldModule, MatSnackBarModule} from '@angular/material';
+import { SnackButtonComponent } from './list/snack-button/snack-button.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ListComponent, SnackButtonComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    CdkStepperModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCardModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  list: any[] = [
+    'Humana EPO-3500',
+    'Aetna NY-2000-XYZ',
+    'Anthem Gold 5500',
+    'Aetna NY-HDHP-4500'
+  ];
+
+  finalList: any[] = ['Humana PPO-5000'];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  saveList() {
+
+  }
+
+  reset() {
+    this.list = [
+      'Humana EPO-3500',
+      'Aetna NY-2000-XYZ',
+      'Anthem Gold 5500',
+      'Aetna NY-HDHP-4500'
+    ];
+
+    this.finalList = ['Humana PPO-5000'];
+  }
+}
