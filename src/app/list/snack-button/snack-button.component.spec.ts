92c1543a6f94aa7b576ddbbd39d689768e528945
Artie Fischer <artie@curetechnologies.us>
2020-05-26 17:00:20 -0600

    buttons active

---
+++ src/app/list/snack-button/snack-button.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SnackButtonComponent } from './snack-button.component';
+
+describe('SnackButtonComponent', () => {
+  let component: SnackButtonComponent;
+  let fixture: ComponentFixture<SnackButtonComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SnackButtonComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SnackButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/list/snack-button/snack-button.component.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material';
+import {ListComponent} from '../list.component';
+
+@Component({
+  selector: 'app-snack-button',
+  templateUrl: './snack-button.component.html',
+  styleUrls: ['./snack-button.component.css']
+})
+export class SnackButtonComponent implements OnInit {
+  @Input() message: string;
+  @Input() action: string;
+  @Input() type: string;
+  @Input() color: string;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+  }
+  openSnackBar() {
+    this.snackBar.open(this.message, this.action, {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    duration: 2000,
+    });
+  }
+}
