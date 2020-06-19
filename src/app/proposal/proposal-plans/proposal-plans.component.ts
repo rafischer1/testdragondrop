742cc8b8255aab72e6fa1b032d9e90a3e0e343ab
Artie Fischer <artiefischer@gmail.com>
2020-06-19 10:31:05 -0600

    wip: filling out proposal bodies - some css problems

--- src/app/proposal/proposal-plans/proposal-plans.component.ts
+++ src/app/proposal/proposal-plans/proposal-plans.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ProposalNavigateService } from "../proposal-navigate.service";
 import { CompaniesService, Company } from "../companies.service";
+import { PlanDesign } from "../../list/list.component";
+import { HelpersService } from "../../services/helpers.service";
 
 @Component({
   selector: "app-proposal-plans",
@@ -11,10 +13,34 @@ import { CompaniesService, Company } from "../companies.service";
 export class ProposalPlansComponent implements OnInit {
   companyID: number;
   company: Company;
+  list: PlanDesign[] = [
+    {
+      name: "Humana EPO-3500",
+      rates: this.help.rates(),
+      renewalDate: "1/1/21",
+      id: 1,
+      locked: false,
+    },
+    {
+      name: "Aetna NY-2000-XYZ",
+      rates: this.help.rates(),
+      renewalDate: "1/1/21",
+      id: 2,
+      locked: false,
+    },
+    {
+      name: "Anthem Gold 5500",
+      rates: this.help.rates(),
+      renewalDate: "1/1/21",
+      id: 3,
+      locked: false,
+    },
+  ];
   constructor(
     private route: ActivatedRoute,
     private service: ProposalNavigateService,
-    private companiesService: CompaniesService
+    private companiesService: CompaniesService,
+    private help: HelpersService
   ) {}
 
   ngOnInit() {
