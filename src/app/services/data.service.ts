8178b08f38dcbc0045cf100691d0b0738515740e
Artie Fischer <artie@curetechnologies.us>
2020-07-07 09:27:05 -0600

    refactor(dataService): on err getting hardcoded plans

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,26 +2,37 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { PlanInput } from "../types/plans";
 import { CompanyInput } from "../types/companies";
+import { PlansService } from "./plans.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class DataService {
   configUrl = "http://localhost:8080";
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private plans: PlansService) {}
 
   getPlans() {
-    this.http.get(this.configUrl + "/plans").subscribe((plans) => {
-      console.log("plans:", plans);
-      return plans;
-    });
+    this.http.get(this.configUrl + "/plans").subscribe(
+      (plans) => {
+        console.log("plans:", plans);
+        return plans;
+      },
+      (err) => {
+        return this.plans.getAll();
+      }
+    );
   }
 
   getPlan(planId: number) {
-    this.http.get(`/plans/:${planId}`).subscribe((plan) => {
-      console.log("get one plan:", plan);
-      return plan;
-    });
+    this.http.get(`/plans/:${planId}`).subscribe(
+      (plan) => {
+        console.log("get one plan:", plan);
+        return plan;
+      },
+      (err) => {
+        return this.plans.get(planId);
+      }
+    );
   }
 
   postPlan(plan: PlanInput) {
@@ -46,17 +57,27 @@ export class DataService {
   }
 
   getAllCompanies() {
-    this.http.get(this.configUrl + "/companies").subscribe((companies) => {
-      console.log("companies:", companies);
-      return companies;
-    });
+    this.http.get(this.configUrl + "/companies").subscribe(
+      (companies) => {
+        console.log("companies:", companies);
+        return companies;
+      },
+      (err) => {
+        console.log("err on companies:", err);
+      }
+    );
   }
 
   getCompany(companyId: number) {
-    this.http.get(`/companies/:${companyId}`).subscribe((company) => {
-      console.log("get one company:", company);
-      return company;
-    });
+    this.http.get(`/companies/:${companyId}`).subscribe(
+      (company) => {
+        console.log("get one company:", company);
+        return company;
+      },
+      (err) => {
+        console.log("err on company:", err);
+      }
+    );
   }
 
   postCompany(company: CompanyInput) {
