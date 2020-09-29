463b8665ae15ef917cc8f8499361d8838dc3aa21
Artie Fischer <artie@curetechnologies.us>
2020-09-29 10:26:43 -0600

    wip(sql): cannot quite get array input to work - might need to be a file upload...

--- src/app/sql-generator/sql-generator.component.ts
+++ src/app/sql-generator/sql-generator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 export class Feature {
   key: string;
@@ -14,27 +15,63 @@ export class Feature {
 export class SqlGeneratorComponent implements OnInit {
   tenantFeatures: string[];
   featuresArr: string[];
+
+  tenantId = new FormControl("");
+  tenantFeaturesFormControl = new FormControl([]);
+  tenantFeaturesDisabled: boolean;
+  tenantForm = new FormGroup({
+    test: new FormControl(""),
+  });
+
+  allFeatures = new FormControl([]);
+  featuresForm = new FormGroup({
+    test: new FormControl(""),
+  });
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.tenantFeaturesDisabled = true;
+  }
 
-  genSQL(features: Feature[], tenantId: string) {
+  genSQL(ev: MouseEvent) {
+    ev.preventDefault();
+    console.log(
+      "GENSQL:",
+      this.tenantId.value,
+      this.tenantFeaturesFormControl.value
+    );
+    const feats = this.tenantFeaturesFormControl.value.split(",");
+    const id = this.tenantId.value;
     this.tenantFeatures = [];
-    features.forEach((f) => {
+    feats.value.forEach((f) => {
       this.tenantFeatures.push(
-        `INSERT INTO api_schema.features_tenant_map ("key", tenant_id, enabled, locked) VALUES ('${f.key}', ${tenantId}, ${f.enabled}, ${f.locked};`
+        `INSERT INTO api_schema.features_tenant_map ("key", tenant_id, enabled, locked) VALUES ('${f.key}', ${id}, ${f.enabled}, ${f.locked};`
       );
     });
     return this.tenantFeatures;
   }
 
-  genAllFeatures(features: Feature[]) {
+  genAllFeatures() {
+    console.log("GEN FEATS:", this.featuresForm.controls.allFeatures.value);
+    const feats = this.allFeatures.value;
     this.featuresArr = [];
-    features.forEach((f) => {
+    feats.value.forEach((f) => {
       this.featuresArr.push(
         `INSERT INTO api_schema.features ("key") VALUES ('${f.key}');`
       );
     });
     return this.featuresArr;
+  }
+
+  areTenantFeaturesDisabled(): boolean {
+    return !!(
+      this.tenantId.value.length > 0 &&
+      this.tenantFeaturesFormControl.value.length > 0
+    );
+  }
+
+  areFeaturesDisabled(): boolean {
+    return this.allFeatures.value.length > 0;
   }
 }
